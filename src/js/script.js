7aa601f181a49a4e4df2b15d6006ba78a6a7c90d
Felipe Kenji Takata <lipe.takata@gmail.com>
2026-03-26 19:36:38 -0300

    feat: Criação do sistema básico em js

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+let nomeCliente = prompt("Digite o seu nome : ");
+let emailCliente = prompt("Digite o seu email: ");
+let nomeVinho = prompt("Digite o nome do vinho: ");
+let tipoVinho = prompt("Informe o Tipo do vinho: ");
+let safraVinho = parseInt(prompt("Informe a safra do vinho: "));
+let qtEstoqueVinho = parseInt(prompt("Informe o estoque do vinho: "));
+alert("Cadastro realizado! Veja os detalhes no console.");
+console.log(`O nome do vinho é ${nomeVinho}`);
+console.log(`O tipo do vinho é ${tipoVinho}`);
+console.log(`A safra do vinho é de ${safraVinho}`);
+console.log(`A quantidade de vinho no estoque é de ${qtEstoqueVinho}`);
+alert("As informações sobre o vinho estão disponíveis no console");
